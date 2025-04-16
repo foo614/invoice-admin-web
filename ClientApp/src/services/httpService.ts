1cf2137dd170db2a343cbdcd9198df2fa1860cdd
Jingyi <jingyi012@gmail.com>
2025-04-16 21:14:21 +0800

    fix: login failed redirect in local

--- ClientApp/src/services/httpService.ts
+++ ClientApp/src/services/httpService.ts
@@ -104,7 +104,7 @@ httpClient.interceptors.response.use(
         localStorage.removeItem('currentUser');
         message.error('Session expired. Please log in again.');
         window.location.href =
-          (process.env.NODE_ENV !== 'development' && '/web-portal') + '/user/login';
+          (process.env.NODE_ENV !== 'development' ? '/web-portal' : '') + '/user/login';
         return Promise.reject(refreshError);
       } finally {
         isRefreshing = false;
